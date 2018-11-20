d845e59f3807dd978cd24dcc36f8b6b386aa2543
Luis Cardenas <lcardenas@doonamis.es>
2018-11-20 16:02:16 +0100

    Initial Commit

---
+++ Klendario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Klendario
+//
+//  Created by Luis Cardenas on 20/11/2018.
+//  Copyright © 2018 ThXou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
